4f1aee3691151da23d23f5e1d3a49606bd79104c
krishna9966 <krishna.9966426434@gmail.com>
2021-02-10 17:55:45 +0530

    customcommand modified

--- src/core/management/commands/rename.py
+++ src/core/management/commands/rename.py
@@ -1,28 +1,35 @@
-from django.core.management.base import BaseCommand
 import os
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Renames a django Project"
+    help = 'Renames a Django project'
 
     def add_arguments(self, parser):
-        parser.add_argument("new_project_name", type=str, help="new project name")
-        # parser.add_argument('-p','--prefix')
+        parser.add_argument('current', type=str, nargs='+',
+                            help='The current Django project folder name')
+        parser.add_argument('new', type=str, nargs='+',
+                            help='The new Django project name')
 
     def handle(self, *args, **kwargs):
-        new_project_name = kwargs['new_project_name']
+        current_project_name = kwargs['current'][0]
+        new_project_name = kwargs['new'][0]
 
-        files_to_rename = ['src/settings/base.py', 'src/wsgi.py', 'manage.py']
-        folder_to_rename = 'src'
+        # logic for renaming the files
+
+        files_to_rename = [f'{current_project_name}/settings/base.py',
+                           f'{current_project_name}/wsgi.py', 'manage.py']
 
         for f in files_to_rename:
             with open(f, 'r') as file:
                 filedata = file.read()
 
-            filedata = filedata.replace('src', new_project_name)
+            filedata = filedata.replace(current_project_name, new_project_name)
 
             with open(f, 'w') as file:
                 file.write(filedata)
 
-        os.rename(folder_to_rename, new_project_name)
-        self.stdout.write(self.style.SUCCESS("project renamed as %s", new_project_name))
+        os.rename(current_project_name, new_project_name)
+
+        self.stdout.write(self.style.SUCCESS(
+            'Project has been renamed to %s' % new_project_name))
