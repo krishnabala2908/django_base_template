c760dca0c9fe473c61f9f100770325e852817489
krishna9966 <krishna.9966426434@gmail.com>
2021-02-19 17:58:44 +0530

    prod settings changes

--- src/core/management/commands/rename.py
+++ src/core/management/commands/rename.py
@@ -17,8 +17,9 @@ class Command(BaseCommand):
 
         # logic for renaming the files
 
-        files_to_rename = [f'{current_project_name}/settings/base.py',
-                           f'{current_project_name}/wsgi.py', f'{current_project_name}/asgi.py', 'manage.py']
+        files_to_rename = [
+            f'{current_project_name}/settings/base.py', f'{current_project_name}/wsgi.py',
+            f'{current_project_name}/asgi.py', 'manage.py', 'pytest.ini']
 
         for f in files_to_rename:
             with open(f, 'r') as file:
